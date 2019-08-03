94c260efc0de6b94bacb63c65eaa470d6d416a63
Monica Parente <mon8806@gmail.com>
2019-08-03 16:46:00 -0700

    add folders

---
+++ src/components/Nav/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+
+// class Nav extends Component{
+//     render(){
+//         return(
+//             <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
+//             <button
+//               onClick={this.toggleNav}
+//               className="navbar-toggler"
+//               data-toggle="collapse"
+//               data-target="#navbarNav"
+//               aria-controls="navbarNav"
+//               aria-expanded="false"
+//               aria-label="Toggle navigation"
+//             >
+//                <span className="navbar-toggler-icon" />
+//         </button>  
+//         )
+//     }
+// }
+
+export default Nav;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import Form from "../components/Nav";
+// import Jumbotron from "../components/Jumbotron";
+// import Card from "../components/Card";
+// import Footer from "../components/Footer";
+
+
+class Home extends Component{
+    render(){
+        return <div>
+            <h1>About Me</h1>
+        </div>
+}
+}
+export default Home;
