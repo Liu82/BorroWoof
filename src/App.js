0bbc6df12afcb2c3fcbbddf76dc8a936fbfd016e
Shawn Liu <shawn_liu82@yahoo.com>
2019-08-04 20:24:54 -0700

    Working on it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Nav from "./components/Nav";
+import UserPage from "./pages/UserPage";
+import DogPage from "./pages/DogPage";
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 
@@ -12,6 +14,8 @@ function App() {
         <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/User" component={UserPage} />
+          <Route exact path="/Dog" component ={DogPage} />
         </Switch>
       </div>
     </Router>
---
+++ src/pages/UserPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Item } from 'semantic-ui-react'
+
+const description = [
+  '.',
+].join(' ')
+
+const ItemExampleDescriptions = () => (
+  <Item.Group>
+    <Item>
+      <Item.Image size='large' src="https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/06/12/104525039-_Y2A6611.600x400.JPG?v=1497302639" wrapped ui={false} />
+
+
+      <Item.Content>
+        <Item.Header as='a'>Clifford Perez</Item.Header>
+        <Item.Description>
+          <p>{description}</p>
+          <p>Dale! Do it! I love Pitbulls.<br></br> <br></br>
+          t is a long established fact that a reader will be distracted by the readable <br></br> 
+          content of a page when looking at its layout. The point of using Lorem Ipsum <br></br>
+          is that it has a more-or-less normal distribution of letters, as opposed to <br></br>
+          using 'Content here, content here', making it look like readable English.<br></br> 
+      </p>
+        </Item.Description>
+      </Item.Content>
+    </Item>
+
+ </Item.Group>
+)
+
+export default ItemExampleDescriptions
